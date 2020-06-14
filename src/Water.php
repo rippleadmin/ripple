f23ac1a0c035b3e801c78e100731ea4dccda8fbc
Lucas Yang <yangchenshin77@gmail.com>
2020-06-15 00:11:56 +0800

    Update Water assets methods

--- src/Water.php
+++ src/Water.php
@@ -7,25 +7,32 @@ use Illuminate\Support\Facades\Route;
 class Water
 {
     /**
-     * The CSS files include to Water Admin.
+     * The CSS assets include to Water Admin.
      *
-     * @var string[]
+     * @var \WaterAdmin\Asset[]
      */
     public static $css = [];
 
     /**
-     * The JS files include to Water Admin.
+     * The JS assets include to Water Admin.
      *
-     * @var string[]
+     * @var \WaterAdmin\Asset[]
      */
     public static $js = [];
 
     /**
-     * The main assets for Water Admin.
+     * The assets for Water Admin.
      *
-     * @var string[]
+     * @var \WaterAdmin\Asset[]
      */
-    public static $mainAssets = [];
+    public static $assets = [];
+
+    /**
+     * The Water Admin plugin routes path.
+     *
+     * @var array
+     */
+    public static $pluginRoutesPath = [];
 
     /**
      * Define the "water" routes for the application.
@@ -36,18 +43,35 @@ class Water
     {
         Route::namespace('\WaterAdmin\Controllers')
             ->group(__DIR__.'/../routes/water.php');
+
+        foreach (static::$pluginRoutesPath as $path) {
+            Route::group([], $path);
+        }
+    }
+
+    /**
+     * Register the plugin routes file path.
+     *
+     * @param  string  $path
+     * @return static
+     */
+    public static function pluginRoutes(string $path)
+    {
+        static::$pluginRoutesPath[] = $path;
+
+        return new static;
     }
 
     /**
      * Add the CSS files include to Water Admin.
      *
-     * @param  string  $name
      * @param  string  $path
+     * @param  string  $manifestDirectory
      * @return static
      */
-    public static function css(string $name, string $path)
+    public static function css(string $path, string $manifestDirectory = '')
     {
-        static::$css[$name] = $path;
+        static::$css[] = new Asset($path, $manifestDirectory);
 
         return new static;
     }
@@ -55,28 +79,63 @@ class Water
     /**
      * Add the JS files include to Water Admin.
      *
-     * @param  string  $name
      * @param  string  $path
+     * @param  string  $manifestDirectory
      * @return static
      */
-    public static function js(string $name, string $path)
+    public static function js(string $path, string $manifestDirectory = '')
     {
-        static::$js[$name] = $path;
+        static::$js[] = new Asset($path, $manifestDirectory);
 
         return new static;
     }
 
     /**
-     * Add the main assets to Water Admin.
+     * Add the asset to Water Admin.
      *
-     * @param  string  $name
      * @param  string  $path
+     * @param  string  $manifestDirectory
      * @return static
      */
-    public static function mainAsset(string $name, string $path)
+    public static function asset(string $path, string $manifestDirectory = '')
     {
-        static::$mainAssets[$name] = $path;
+        static::$assets[] = new Asset($path, $manifestDirectory);
 
         return new static;
+    }
+
+    /**
+     * Get the Water Admin styles assets instance.
+     *
+     * @return array
+     */
+    public static function styles()
+    {
+        return static::$css;
+    }
+
+    /**
+     * Get the Water Admin scripts assets instance.
+     *
+     * @return array
+     */
+    public static function scripts()
+    {
+        return static::$js;
+    }
+
+    /**
+     * Get the Water Admin asset instance.
+     *
+     * @param  string  $path
+     * @return \WaterAdmin\Asset|null
+     */
+    protected function getAsset(string $path)
+    {
+        return array_merge(
+            static::$assets,
+            static::$css,
+            static::$js
+        )[$path] ?? null;
     }
 }
