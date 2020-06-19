419cd7c0cda91f154669dc1955967317035190dd
Lucas Yang <yangchenshin77@gmail.com>
2020-06-19 10:48:18 +0800

    Remove test vue components

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -3,14 +3,11 @@
 namespace WaterAdmin\Controllers;
 
 use Inertia\Inertia;
-use WaterAdmin\Components\Aaa;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Home', [
-            'aaa' => Aaa::make(),
-        ]);
+        return Inertia::render('Home');
     }
 }
